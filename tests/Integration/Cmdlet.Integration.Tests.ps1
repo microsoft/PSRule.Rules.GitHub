cb80c3f0ef2bdd09e0d03fb686125f39e3bac706
Bernie White <bewhite@microsoft.com>
2020-07-30 12:45:50 +1000

    Update integration tests

--- tests/Integration/Cmdlet.Integration.Tests.ps1
+++ tests/Integration/Cmdlet.Integration.Tests.ps1
@@ -23,12 +23,12 @@ $outputPath = Join-Path -Path $rootPath -ChildPath out/tests/PSRule.Rules.GitHub
 Remove-Item -Path $outputPath -Force -Recurse -Confirm:$False -ErrorAction Ignore;
 $Null = New-Item -Path $outputPath -ItemType Directory -Force;
 
-#region Export-AzRuleData
+#region Export-GitHubRuleData
 
 Describe 'Export-GitHubRuleData' -Tag 'Cmdlet','Export-GitHubRuleData' {
     Context 'With defaults' {
         It 'Exports repository data' {
-            $results = @(Export-GitHubRuleData -OutputPath $outputPath -R Microsoft/PSRule);
+            $results = @(Export-GitHubRuleData -OutputPath $outputPath -R Microsoft/PSRule,BernieWhite/PSRule.Rules.GitHub);
             $results | Should -Not -BeNullOrEmpty;
             $results | Should -BeOfType System.IO.FileInfo;
             $jsonResults = Get-Content -Path $results.FullName -Raw | ConvertFrom-Json;
@@ -36,19 +36,19 @@ Describe 'Export-GitHubRuleData' -Tag 'Cmdlet','Export-GitHubRuleData' {
             # Get repository
             $filteredResults = @($jsonResults | Where-Object { $_.Type -eq 'api.github.com/repos' });
             $filteredResults | Should -Not -BeNullOrEmpty;
-            $filteredResults.Length | Should -Be 1;
-            $filteredResults.Name | Should -BeIn 'PSRule';
-            $filteredResults.Owner | Should -BeIn 'microsoft';
+            $filteredResults.Length | Should -Be 2;
+            $filteredResults.Name | Should -BeIn 'PSRule', 'PSRule.Rules.GitHub';
+            $filteredResults.Owner | Should -BeIn 'microsoft', 'BernieWhite';
             $filteredResults.DefaultBranch | Should -BeIn 'main';
             $filteredResults.Branches | Should -Not -BeNullOrEmpty;
-            $filteredResults.Branches.Length | Should -BeGreaterOrEqual 2;
+            $filteredResults.Branches.Length | Should -BeGreaterOrEqual 3;
 
             # Get branches
             $filteredResults = @($jsonResults | Where-Object { $_.Type -eq 'api.github.com/repos/branches' });
             $filteredResults | Should -Not -BeNullOrEmpty;
-            $filteredResults.Length | Should -BeGreaterOrEqual 2;
+            $filteredResults.Length | Should -BeGreaterOrEqual 3;
         }
     }
 }
 
-#endregion Export-AzRuleData
+#endregion Export-GitHubRuleData
