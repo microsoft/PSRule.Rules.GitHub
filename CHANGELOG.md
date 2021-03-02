336b4f3a09f9a268040a9197b019b406583959f1
Bernie White <bewhite@microsoft.com>
2021-03-03 00:50:33 +1000

    Bump PSRule to v1.1.0 #41

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,14 @@
 
 ## Unreleased
 
+What's changed since pre-release v0.1.0-B2009006:
+
+- Engineering:
+  - Bump PSRule dependency to v1.1.0. [#41](https://github.com/microsoft/PSRule.Rules.GitHub/issues/41)
+    - See the [change log](https://github.com/microsoft/PSRule/blob/main/docs/CHANGELOG-v1.md#v110)
+  - Bump Octokit to 0.50.0. [#50](https://github.com/microsoft/PSRule.Rules.GitHub/pull/50)
+  - Bump YamlDotNet to 9.1.4. [#46](https://github.com/microsoft/PSRule.Rules.GitHub/pull/46)
+
 ## v0.1.0-B2009006 (pre-release)
 
 - Initial pre-release.
--- pipeline.build.ps1
+++ pipeline.build.ps1
@@ -108,7 +108,7 @@ task VersionModule ModuleDependencies, {
     $manifest = Test-ModuleManifest -Path $manifestPath;
     $requiredModules = $manifest.RequiredModules | ForEach-Object -Process {
         if ($_.Name -eq 'PSRule' -and $Configuration -eq 'Release') {
-            @{ ModuleName = 'PSRule'; ModuleVersion = '0.20.0' }
+            @{ ModuleName = 'PSRule'; ModuleVersion = '1.1.0' }
         }
         else {
             @{ ModuleName = $_.Name; ModuleVersion = $_.Version }
@@ -155,16 +155,16 @@ task PSScriptAnalyzer NuGet, {
 
 # Synopsis: Install PSRule
 task PSRule NuGet, {
-    if ($Null -eq (Get-InstalledModule -Name PSRule -MinimumVersion 0.20.0 -ErrorAction Ignore)) {
-        Install-Module -Name PSRule -Repository PSGallery -MinimumVersion 0.20.0 -Scope CurrentUser -Force;
+    if ($Null -eq (Get-InstalledModule -Name PSRule -MinimumVersion 1.1.0 -ErrorAction Ignore)) {
+        Install-Module -Name PSRule -Repository PSGallery -MinimumVersion 1.1.0 -Scope CurrentUser -Force;
     }
     Import-Module -Name PSRule -Verbose:$False;
 }
 
 # Synopsis: Install PSDocs
 task PSDocs NuGet, {
-    if ($Null -eq (Get-InstalledModule -Name PSDocs -MinimumVersion 0.6.3 -ErrorAction Ignore)) {
-        Install-Module -Name PSDocs -Repository PSGallery -MinimumVersion 0.6.3 -Scope CurrentUser -Force;
+    if ($Null -eq (Get-InstalledModule -Name PSDocs -MinimumVersion 0.8.0 -ErrorAction Ignore)) {
+        Install-Module -Name PSDocs -Repository PSGallery -MinimumVersion 0.8.0 -Scope CurrentUser -Force;
     }
     Import-Module -Name PSDocs -Verbose:$False;
 }
